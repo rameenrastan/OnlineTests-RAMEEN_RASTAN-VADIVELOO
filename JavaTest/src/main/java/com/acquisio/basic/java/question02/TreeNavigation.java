2fa32ad02273925d2f5c849d4467bbd20e9860d2
Rameen Rastan <rameenrastanv@hotmail.com>
2017-02-21 12:06:40 -0500

    iterate over children nodes to print the nodes based on their depth

--- JavaTest/src/main/java/com/acquisio/basic/java/question02/TreeNavigation.java
+++ JavaTest/src/main/java/com/acquisio/basic/java/question02/TreeNavigation.java
@@ -31,5 +31,36 @@ public class TreeNavigation {
         Node n4 = new Node("5", null, n3);
         Node n = new Node("2", n2, n4);
         // TODO: Implement code here
+        
+        System.out.println(".." +n.name);
+        System.out.println("...." + n.children[0].name);
+        
+        //iterates through children of left node
+        for(Node leftNodeChild : n.children[0].children){
+        	
+        	System.out.println("......" + leftNodeChild.name);
+        	
+        	for(Node child : leftNodeChild.children){
+        		
+        		System.out.println("........" + child.name);
+        		
+        	}
+        	
+        }
+        System.out.println("...." + n.children[1].name);
+        
+        //iterates through children of right node
+        for(Node rightNodeChild : n.children[1].children){
+        	
+        	if(rightNodeChild != null){
+        	System.out.println("......" + rightNodeChild.name);
+        	
+        	for(Node child : rightNodeChild.children){
+        		
+        		System.out.println("........" + child.name);
+        		
+        	}}
+        	
+        }
     }
 }
