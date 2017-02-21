a30cb775dc955c452a05ed3ec6ec3d2c4c071a2a
Rameen Rastan <rameenrastanv@hotmail.com>
2017-02-21 12:52:27 -0500

    implemented recursive method that converts integer to binary as string

--- JavaTest/src/main/java/com/acquisio/basic/java/question03/ConvertToBinary.java
+++ JavaTest/src/main/java/com/acquisio/basic/java/question03/ConvertToBinary.java
@@ -14,16 +14,22 @@ public class ConvertToBinary {
 
     public static void main(String[] args) {
         ConvertToBinary instance = new ConvertToBinary();
-       instance.convertToBinary(17);
+        System.out.println(instance.convertToBinary(17));
     }
 
-    private void convertToBinary(int val) {
+    private String convertToBinary(int val) {
     	
-    	if(val > 0){
+    	//base case
+    	if(val==0){
     		
-    		convertToBinary(val/2);
+    		return "";
     		
-    		System.out.print(val % 2 + "");
+    	}
+    	//recursive call (val%2 will return 1 for odd numbers and 0 for even numbers), append this to string 
+    	//then divide by two and repeat the call
+    	else{
+    		
+    		return convertToBinary(val/2) + Integer.toString(val % 2); 
     		
     	}
     }
