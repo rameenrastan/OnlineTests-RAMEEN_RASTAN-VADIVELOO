5d1c429c4b626b8d7786d61a9e654ec2cb708d72
Rameen Rastan <rameenrastanv@hotmail.com>
2017-02-20 23:24:52 -0500

    added CalculatorTest JUnit test case class

---
+++ JavaTest/src/main/java/com/acquisio/basic/java/question08/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.acquisio.basic.java.question08;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
