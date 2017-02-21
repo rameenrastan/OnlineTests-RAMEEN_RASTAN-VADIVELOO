0876fc331d67b21e19645106f92d4c2bf71d41f2
Rameen Rastan <rameenrastanv@hotmail.com>
2017-02-20 23:45:27 -0500

    added CalculatorTest test suite to view all test results

---
+++ JavaTest/src/main/java/com/acquisio/basic/java/question08/CalculatorTest.java
@@ -0,0 +1,11 @@
+package com.acquisio.basic.java.question08;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AdditionTest.class, DivisionTest.class, MultiplicationTest.class, SubtractionTest.class })
+public class CalculatorTest {
+
+}
